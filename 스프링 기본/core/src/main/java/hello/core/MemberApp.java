945ceee84f95bae24e9c3685ae0a7e98bf30da56
HyunRian <121999215+hyunrian@users.noreply.github.com>
2023-12-11 17:53:20 +0900

    핵심 원리: 객체 지향 원리 적용 - AppConfig와 DI

--- 스프링 기본/core/src/main/java/hello/core/MemberApp.java
+++ 스프링 기본/core/src/main/java/hello/core/MemberApp.java
@@ -3,12 +3,21 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+
+//        AppConfig appConfig = new AppConfig(); // 직접 객체 생성하고 DI를 하는 방법
+//        MemberService memberService = appConfig.memberService();
+
+        // 스프링 컨테이너. 객체를 관리하는 역할
+        // 파라미터로 넣은 AppConfig에 있는 설정 정보를 토대로(@Bean) 스프링 컨테이너에 등록함
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
