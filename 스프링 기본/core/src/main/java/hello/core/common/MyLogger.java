ad90a276fc1cab89b12e1e8d113fdfe2d0570486
HyunRian <121999215+hyunrian@users.noreply.github.com>
2023-12-19 18:06:21 +0900

    Bean Scope - Request Scope(Scope와 Proxy)

--- 스프링 기본/core/src/main/java/hello/core/common/MyLogger.java
+++ 스프링 기본/core/src/main/java/hello/core/common/MyLogger.java
@@ -3,12 +3,17 @@ package hello.core.common;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
 @Component
-@Scope(value = "request") // client의 request별로 하나씩 생성됨(실제 요청이 왔을 때 생성)
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+// client의 request별로 하나씩 생성됨(실제 요청이 왔을 때 생성)
+// proxyMode를 추가하면 MyLogger의 가짜 프록시 클래스를 만들어 두고 HTTP request와 상관없이 가짜 프록시 클래스를 다른 빈에 미리 주입해 둘 수 있음
+// -> 적용 대상이 클래스면 TARGET_CLASS, 인터페이스면 INTERFACES 선택
+// 무분별하게 사용하면 유지보수가 어려우므로 반드시 필요한 곳에서 최소화로 사용해야 함
 public class MyLogger {
 
     private String uuid;
--- 스프링 기본/core/src/main/java/hello/core/web/LogDemoController.java
+++ 스프링 기본/core/src/main/java/hello/core/web/LogDemoController.java
@@ -13,13 +13,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider; // 실제 요청이 올 때가 아닌 주입 시점에서 생성되도록 함
+//    private final ObjectProvider<MyLogger> myLoggerProvider; // 실제 요청이 올 때가 아닌 주입 시점에서 생성되도록 함
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject(); // 이 시점까지 request scope 빈의 생성 지연
+//        MyLogger myLogger = myLoggerProvider.getObject(); // 이 시점까지 request scope 빈의 생성 지연
+        System.out.println("myLogger = " + myLogger.getClass()); // class hello.core.common.MyLogger$$SpringCGLIB$$0
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
