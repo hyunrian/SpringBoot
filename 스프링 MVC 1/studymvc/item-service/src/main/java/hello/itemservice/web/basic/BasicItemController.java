c5c58bd8f2065c35f582863f5d6c6400e05a08b9
HyunRian <121999215+hyunrian@users.noreply.github.com>
2024-01-02 19:58:53 +0900

    웹페이지 만들기 - 상품 등록 폼과 등록 처리(@ModelAttribute)

--- 스프링 MVC 1/studymvc/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ 스프링 MVC 1/studymvc/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,9 +6,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -42,4 +40,29 @@ public class BasicItemController {
         itemRepository.save(new Item("itemB", 20000, 20));
     }
 
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV1(@ModelAttribute("item") Item item, Model model) {
+        itemRepository.save(item);
+//        model.addAttribute("item", item); // ModelAttribute name을 지정하면 해당 이름으로 자동 추가됨
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        // ModelAttribute name을 지정하지 않으면 클래스명의 첫글자를 소문자로 바꾼 이름으로 자동 추가됨 ex) Item -> item
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV3(Item item) {
+        itemRepository.save(item);
+        // @ModelAttribute 자체를 생략해도 클래스명 첫글자를 소문자로 바꾼 이름으로 자동 추가됨
+        return "basic/item";
+    }
 }
