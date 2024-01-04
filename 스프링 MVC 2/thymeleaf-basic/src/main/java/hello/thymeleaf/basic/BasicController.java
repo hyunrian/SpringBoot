c9dd79db5d99ac527d505ff95f6e8ec8a058d748
HyunRian <121999215+hyunrian@users.noreply.github.com>
2024-01-04 18:36:26 +0900

    타임리프 - 자바스크립트 인라인

--- 스프링 MVC 2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ 스프링 MVC 2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -124,6 +124,13 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("newUser", 11));
+        addUsers(model);
+        return "basic/javascript";
+    }
+
     private void addUsers(Model model) {
 
         List<User> list = new ArrayList<>();
