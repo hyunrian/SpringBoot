2099d383e7e13b99e38ddc4bc851343475c513b1
HyunRian <121999215+hyunrian@users.noreply.github.com>
2024-01-09 15:03:27 +0900

    validation - 오류 코드와 메시지 처리
    errors.properties와 BindingResult

--- 스프링 MVC 2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ 스프링 MVC 2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -84,7 +84,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         // BindingResult: 스프링이 제공하는, 검증 오류를 보관하는 객체. 검증 오류가 발생하면 여기에 보관하면 됨
         // BindingResult는 검증할 대상(@ModelAttirbute 객체) 바로 다음에 와야 함
@@ -94,7 +94,7 @@ public class ValidationItemControllerV2 {
         // 검증 로직
         if (!StringUtils.hasText(item.getItemName())) {
             bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, null, null, "샹품 이름은 필수입니다."));
-            // FieldError 생성자의 파라미터 중 rejectedValue가 있으면 사용자가 잘못된 값을 입력하고 저장버튼을 눌러도 기존 입력했던 값이 남아있게 됨
+            // FieldError 생성자의 파라미터 중 rejectedValue가 있으면 사용자가 잘못된 값을 입력하고 저장버튼을 눌러도 기존 입력했던 값이 남아있게
         }
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
             bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null, "가격은 1,000~1,000,000까지 허용합니다."));
@@ -108,6 +108,47 @@ public class ValidationItemControllerV2 {
             int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000) {
                 bindingResult.addError(new ObjectError("item", null, null, "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
+            }
+        }
+
+        // 검증에 실패하면 다시 입력폼으로 이동
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v2/addForm"; // bindingReasult는 자동으로 model에 담김
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        // BindingResult: 스프링이 제공하는, 검증 오류를 보관하는 객체. 검증 오류가 발생하면 여기에 보관하면 됨
+        // BindingResult는 검증할 대상(@ModelAttirbute 객체) 바로 다음에 와야 함
+        // BindingResult가 있으면 타입오류(Integer타입 <input>애 문자 입력)가 있어도 컨트롤러가 정상적으로 호출됨
+
+        // 검증 로직
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, new String[]{"required.item.itemName"}, null, null));
+            // FieldError 생성자의 파라미터 중 rejectedValue가 있으면 사용자가 잘못된 값을 입력하고 저장버튼을 눌러도 기존 입력했던 값이 남아있게 됨
+            // codes: errors.properties에서 설정한 이름을 사용하여 메시지 코드 지정
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, new String[]{"range.item.price"}, new Object[]{1000, 1000000}, null));
+            // arguments: errors.properties에서 설정한 {0}, {1}을 치환할 값 전달
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, new String[]{"max.item.quantity"}, new Object[]{9999}, null));
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"}, new Object[]{10000, resultPrice}, null));
             }
         }
 
