c200e81e213e9e8393b00655a12486c89e51c83c
HyunRian <121999215+hyunrian@users.noreply.github.com>
2024-01-09 19:01:09 +0900

    validation - Bean Validation 살펴보기

--- 스프링 MVC 2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ 스프링 MVC 2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,26 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class Item {
 
     private Long id;
+
+    @NotBlank(message = "공백X") // 빈값 또는 공백만 있는 경우 허용 불가
     private String itemName;
+
+    @NotNull // null 허용 불가
+    @Range(min = 1000, max = 1000000) // hibernate validator 구현체를 사용할 때만 제공. 실무에서 대부분 사용
     private Integer price;
+
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     public Item() {
