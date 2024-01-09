8ddd6a8958a559b6ea416830a1842f443b668f59
HyunRian <121999215+hyunrian@users.noreply.github.com>
2024-01-09 16:34:01 +0900

    validation - 오류 코드와 메시지 처리(level에 따른 처리)
    - errors.properties에 레벨에 따른 코드와 메시지를 정리
    - MessageCodesResolver의 역할 이해

--- 스프링 MVC 2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ 스프링 MVC 2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -10,6 +10,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -124,11 +125,14 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         // BindingResult: 스프링이 제공하는, 검증 오류를 보관하는 객체. 검증 오류가 발생하면 여기에 보관하면 됨
         // BindingResult는 검증할 대상(@ModelAttirbute 객체) 바로 다음에 와야 함
         // BindingResult가 있으면 타입오류(Integer타입 <input>애 문자 입력)가 있어도 컨트롤러가 정상적으로 호출됨
+
+        log.info("objectName={}", bindingResult.getObjectName());
+        log.info("target={}", bindingResult.getTarget());
 
         // 검증 로직
         if (!StringUtils.hasText(item.getItemName())) {
@@ -149,6 +153,50 @@ public class ValidationItemControllerV2 {
             int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000) {
                 bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"}, new Object[]{10000, resultPrice}, null));
+            }
+        }
+
+        // 검증에 실패하면 다시 입력폼으로 이동
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v2/addForm"; // bindingReasult는 자동으로 model에 담김
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        // BindingResult: 스프링이 제공하는, 검증 오류를 보관하는 객체. 검증 오류가 발생하면 여기에 보관하면 됨
+        // BindingResult는 검증할 대상(@ModelAttirbute 객체) 바로 다음에 와야 함
+        // BindingResult가 있으면 타입오류(Integer타입 <input>애 문자 입력)가 있어도 컨트롤러가 정상적으로 호출됨
+
+        log.info("objectName={}", bindingResult.getObjectName());
+        log.info("target={}", bindingResult.getTarget());
+
+        // 검증 로직
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindingResult.rejectValue("itemName", "required");
+            // errorCode: errors.properties에 등록한 코드와 다름. messageCodesResolver를 위한 오류 코드
+        }
+//        ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "itemName", "required"); // 위의 코드와 같음. empty나 공백 같은 단순한 기능만 제공함
+
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
         }
 
