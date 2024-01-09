b270817c73f60d32b0c79f8840df0c12a61b9a93
HyunRian <121999215+hyunrian@users.noreply.github.com>
2024-01-09 20:56:37 +0900

    Bean Validation - Form 전송 객체 분리

--- 스프링 MVC 2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ 스프링 MVC 2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,29 +1,13 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
-import org.hibernate.validator.constraints.Range;
-import org.hibernate.validator.constraints.ScriptAssert;
-
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Data
-//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "금액과 수량의 곱이 10,000원 이상으로 입력하세요.")
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class) // 수정 요구사항 추가
     private Long id;
-
-    @NotBlank(message = "공백X", groups = {SaveCheck.class, UpdateCheck.class}) // 빈값 또는 공백만 있는 경우 허용 불가
     private String itemName;
-
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class}) // null 허용 불가
-    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class}) // hibernate validator 구현체를 사용할 때만 제공. 실무에서 대부분 사용
     private Integer price;
-
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
---
+++ 스프링 MVC 2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV4.java
@@ -0,0 +1,114 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.SaveCheck;
+import hello.itemservice.domain.item.UpdateCheck;
+import hello.itemservice.web.validation.form.ItemSaveForm;
+import hello.itemservice.web.validation.form.ItemUpdateForm;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/validation/v4/items")
+@RequiredArgsConstructor
+public class ValidationItemControllerV4 {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "validation/v4/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v4/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "validation/v4/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(@Validated @ModelAttribute("item") ItemSaveForm form, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        // @Validate(스프링 전용)를 넣으면 Item에 대해 자동으로 검증기가 수행됨. 검증 대상 앞에 쓰면 됨
+        // @Valid(자바 표준)를 써도 똑같이 적용됨(설정은 따로 필요)
+
+        // 특정 필드가 아닌 복합 룰 검증
+        // Item 클래스에 @ScriptAssert를 적용해도 되지만 제약이 많아 Object 오류는 자바 코드로 직접 처리하는 것을 권장
+        if (form.getPrice() != null && form.getQuantity() != null) {
+            int resultPrice = form.getPrice() * form.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        // 검증에 실패하면 다시 입력폼으로 이동
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v4/addForm"; // bindingResult는 자동으로 model에 담김
+        }
+
+        // 성공 로직
+        Item item = new Item();
+        item.setItemName(form.getItemName());
+        item.setPrice(form.getPrice());
+        item.setQuantity(form.getQuantity());
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v4/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v4/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute("item") ItemUpdateForm form, BindingResult bindingResult) {
+
+        // 특정 필드가 아닌 복합 룰 검증
+        // Item 클래스에 @ScriptAssert를 적용해도 되지만 제약이 많아 Object 오류는 자바 코드로 직접 처리하는 것을 권장
+        if (form.getPrice() != null && form.getQuantity() != null) {
+            int resultPrice = form.getPrice() * form.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v4/editForm"; // bindingResult는 자동으로 model에 담김
+        }
+
+        Item updateParam = new Item();
+        updateParam.setItemName(form.getItemName());
+        updateParam.setPrice(form.getPrice());
+        updateParam.setQuantity(form.getQuantity());
+
+        itemRepository.update(itemId, updateParam);
+        return "redirect:/validation/v4/items/{itemId}";
+    }
+
+}
+
