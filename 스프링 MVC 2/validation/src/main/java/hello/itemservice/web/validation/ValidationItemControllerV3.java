1002cb3d9c050141790f3a50e195eef542613de5
HyunRian <121999215+hyunrian@users.noreply.github.com>
2024-01-09 20:22:35 +0900

    Bean Validation - 수정 시 검증 추가, groups

--- 스프링 MVC 2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ 스프링 MVC 2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -2,6 +2,8 @@ package hello.itemservice.web.validation;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.SaveCheck;
+import hello.itemservice.domain.item.UpdateCheck;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -41,15 +43,51 @@ public class ValidationItemControllerV3 {
         return "validation/v3/addForm";
     }
 
-    @PostMapping("/add")
-    public String addItemV(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+//    @PostMapping("/add")
+    public String addItem(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         // @Validate(스프링 전용)를 넣으면 Item에 대해 자동으로 검증기가 수행됨. 검증 대상 앞에 쓰면 됨
         // @Valid(자바 표준)를 써도 똑같이 적용됨(설정은 따로 필요)
+
+        // 특정 필드가 아닌 복합 룰 검증
+        // Item 클래스에 @ScriptAssert를 적용해도 되지만 제약이 많아 Object 오류는 자바 코드로 직접 처리하는 것을 권장
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
 
         // 검증에 실패하면 다시 입력폼으로 이동
         if (bindingResult.hasErrors()) {
             log.info("errors={}", bindingResult);
-            return "validation/v3/addForm"; // bindingReasult는 자동으로 model에 담김
+            return "validation/v3/addForm"; // bindingResult는 자동으로 model에 담김
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV2(@Validated(SaveCheck.class) @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        // @Validate(스프링 전용)를 넣으면 Item에 대해 자동으로 검증기가 수행됨. 검증 대상 앞에 쓰면 됨
+        // @Valid(자바 표준)를 써도 똑같이 적용됨(설정은 따로 필요)
+
+        // 특정 필드가 아닌 복합 룰 검증
+        // Item 클래스에 @ScriptAssert를 적용해도 되지만 제약이 많아 Object 오류는 자바 코드로 직접 처리하는 것을 권장
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        // 검증에 실패하면 다시 입력폼으로 이동
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v3/addForm"; // bindingResult는 자동으로 model에 담김
         }
 
         // 성공 로직
@@ -66,8 +104,44 @@ public class ValidationItemControllerV3 {
         return "validation/v3/editForm";
     }
 
+//    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
+
+        // 특정 필드가 아닌 복합 룰 검증
+        // Item 클래스에 @ScriptAssert를 적용해도 되지만 제약이 많아 Object 오류는 자바 코드로 직접 처리하는 것을 권장
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v3/editForm"; // bindingResult는 자동으로 model에 담김
+        }
+
+        itemRepository.update(itemId, item);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
     @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+    public String editV2(@PathVariable Long itemId, @Validated(UpdateCheck.class) @ModelAttribute Item item, BindingResult bindingResult) {
+
+        // 특정 필드가 아닌 복합 룰 검증
+        // Item 클래스에 @ScriptAssert를 적용해도 되지만 제약이 많아 Object 오류는 자바 코드로 직접 처리하는 것을 권장
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v3/editForm"; // bindingResult는 자동으로 model에 담김
+        }
+
         itemRepository.update(itemId, item);
         return "redirect:/validation/v3/items/{itemId}";
     }
