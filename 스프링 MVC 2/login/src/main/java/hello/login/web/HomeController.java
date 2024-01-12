e984627ae8fe226aabdc618e8183f608f41340fb
HyunRian <121999215+hyunrian@users.noreply.github.com>
2024-01-12 13:40:29 +0900

    로그인 처리 - ArgumentResolver(@Login 세션에 넣은 객체 쉽게 활용)

--- 스프링 MVC 2/login/src/main/java/hello/login/web/HomeController.java
+++ 스프링 MVC 2/login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentresolver.Login;
 import hello.login.web.session.SessionManager;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -80,10 +81,30 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV3Spring(
             @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
         // @SessionAttribute: 세션을 편하게 관리할 수 있음. 세션을 생성하는 기능은 없음
+
+        // 세션에 회원 데이터가 없으면 홈으로
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션이 유지되면 로그인된 페이지로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3ArgumentResolver(@Login Member loginMember, Model model) {
+        // @Login: ArgumentResolver를 이용하여 직접 애너테이션을 만들고 등록함
+        // 공통작업이 필요할 때 컨트롤러를 더욱 편리하게 사용할 수 있음
+        // 컨트롤러 호출 직전에 호출되어서 필요한 파라미터 정보를 생성해줌
+        // 여기서는 세션에 있는 로그인 회원 정보인 Member 객체를 찾아서 반환해 줌
+        // argumentresolver/Login - 애너테이션 생성
+        // argumentresolver/LoginMemberArgumentResolver - 애너테이션 설정
+        // WebConfig - 애너테이션 등록
 
         // 세션에 회원 데이터가 없으면 홈으로
         if (loginMember == null) {
