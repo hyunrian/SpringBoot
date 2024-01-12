b8279b434231858326c8765ac0717f3c20555fc3
HyunRian <121999215+hyunrian@users.noreply.github.com>
2024-01-12 22:30:29 +0900

    서블릿 예외 처리 - 인터셉터

--- 스프링 MVC 2/exception/src/main/java/hello/exception/WebConfig.java
+++ 스프링 MVC 2/exception/src/main/java/hello/exception/WebConfig.java
@@ -1,23 +1,34 @@
 package hello.exception;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**"); // 오류 페이지 경로까지 적용되지 않도록 설정 필요
+        // 설정하지 않으면 오류 원래 인터셉터, 오류 발생 시 호출되는 인터셉터 호출 -> 중복 호출됨
+    }
+
+//    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
         filterRegistrationBean.setOrder(1);
         filterRegistrationBean.addUrlPatterns("/*");
-//        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
+//        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR); // 디폴트 설정이 REQUEST만 처리하도록 되어 있음
         return filterRegistrationBean;
     }
 }
