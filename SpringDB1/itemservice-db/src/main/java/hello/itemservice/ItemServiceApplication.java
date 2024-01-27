1c3201211ed728d7d593c9ff9d28165ee807408c
HyunRian <121999215+hyunrian@users.noreply.github.com>
2024-01-27 16:10:37 +0900

    DB와 테스트 - @Transactional, Embeded Mode

--- SpringDB1/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ SpringDB1/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,13 +2,17 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
 
+@Slf4j
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
@@ -26,5 +30,19 @@ public class ItemServiceApplication {
 //	public TestDataInit testDataInit(ItemRepository itemRepository) {
 //		return new TestDataInit(itemRepository);
 //	}
+
+/*
+	@Bean
+	@Profile("test") // profile이 test인 경우 DataSource를 빈으로 직접 등록해서 사용
+	public DataSource dataSource() {
+		log.info("메모리 데이터베이스 초기화");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		// 임베디드 모드(메모리 모드)로 동작하는 h2 데이터베이스 사용
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+		dataSource.setUsername("");
+		return dataSource;
+	}
+*/
 
 }
